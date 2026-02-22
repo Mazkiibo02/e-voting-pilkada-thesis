6770d8236f326c1a1051edc53aed7acbec9aef34
Mazkiibo02 <hafidzr.smkn3tgl@gmail.com>
2026-02-23 02:52:38 +0700

    be-api-fe

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { contract } from "./services/blockchain";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "E-Voting Backend Running" });
+});
+
+app.get("/candidates/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const candidate = await contract.getCandidate(id);
+
+    res.json({
+      id: Number(candidate[0]),
+      name: candidate[1],
+      voteCount: Number(candidate[2]),
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch candidate" });
+  }
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
